acb77e39fc93d2d5bdf4b31858691916b417754a
JaeYoonJung08 <whitecy01@naver.com>
2024-05-05 18:02:14 +0900

    게시글 수정 페이지, 처리

--- src/main/java/com/study/board/entity/Board.java
+++ src/main/java/com/study/board/entity/Board.java
@@ -21,4 +21,12 @@ public class Board {
     public String getTitle() {
         return title;
     }
+    public String getContent(){return  content;}
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
